4a686dbd8cbfd5d0d7fc33268c2040b03cca8690
bulochka <elina.paxar@gmail.com>
2022-06-04 19:02:02 +0300

    some fix

--- SQL_Statements.py
+++ SQL_Statements.py
@@ -1,15 +1,18 @@
-#!./venv/bin/python3
+#!/usr/bin/env python3
+
 import psycopg2
 import time
-from diogram import Graph
+from diagram import Graph
 from clickhouse_driver import Client
 
-DBNAME = "postgres";
-USER = "postgres";
-PASSWORD = "amin111";
-HOST = "127.0.0.1";
+DBNAME = "perfdb"
+USER = "admin"
+PASSWORD = "password"
+HOST = "127.0.0.1"
 
 client = Client('localhost')
+client.execute("use perfdb")
+
 
 def analiz(a):
     max = a[0];
@@ -29,53 +32,56 @@ def pg_select(conn,i):
 	try:
 		file = open('SQL/test{}.sql'.format(i+1), encoding='utf-8')
 		s = file.read();
-		for j in range(10):
-			cursor = conn.cursor();
-			start_time = time.time();
-			cursor.execute(s);
+		for j in range(50):
+			cursor = conn.cursor()
+			start_time = time.time()
+			cursor.execute(s)
 			#print("--- %s seconds ---" % (time.time() - start_time))
-			t.append(time.time() - start_time);	
-		t = analiz(t);
+			t.append(time.time() - start_time)
+		t = analiz(t)
 	except Exception as _ex:
-		print ("[INFO] Error while working with PostgreSQL",_ex);
+		print ("[INFO] Error while working with PostgreSQL",_ex)
 	finally:
 		if conn:
-			cursor.close();
-			print("[INFO] PostgreSQL connection closed");
-			return t;
+			cursor.close()
+			print("[INFO] PostgreSQL connection closed")
+			return t
 
 
 def ch_select(i):
-	t = [];
-	try:
-		file = open('SQL/test{}.sql'.format(i+1), encoding='utf-8')
-		s = file.read();
-		for j in range(10):
-			start_time = time.time();
-			client.execute(s);
-			#print("--- %s seconds ---" % (time.time() - start_time))
-			t.append(time.time() - start_time);	
-		t = analiz(t);
-	except Exception as _ex:
-		print ("[INFO] Error ",_ex);
-	finally:
-		if client:
-			return t;
-
+    t = []
+    try:
+        file = open('SQL/test{}.sql'.format(i+1), encoding='utf-8')
+        s = file.read().replace('TO_DATE', 'toDate')
+        for j in range(50):
+            start_time = time.time()
+            client.execute(s)
+            #print("--- %s seconds ---" % (time.time() - start_time))
+            t.append(time.time() - start_time)
+        t = analiz(t)
+    except Exception as _ex:
+        print("[INFO] Error", _ex)
+    finally:
+        if client:
+            return t
 
 
 if __name__ == "__main__": 
     conn = psycopg2.connect(dbname=DBNAME, user=USER, password=PASSWORD, host=HOST)	
 
-    postgres_time = [];
-    ch_time = [];
+    postgres_time = []
+    ch_time = []
 
-    for i in range(10):
-        postgres_time.append(pg_select(conn,i));
-        ch_time.append(ch_select(i));
+    for i in range(5):
+        postgres_time.append(pg_select(conn,i))
+        ch_time.append(ch_select(i))
+    
+    print(postgres_time)
+    print(ch_time)
 
-    rects_values = [[postgres_time],[ch_time]];
+    rects_values = [postgres_time,ch_time];
     graph = Graph(rects_values)
     graph.create_graph()
+    
     conn.commit()
     conn.close()
--- script.py
+++ script.py
@@ -1,34 +1,27 @@
-#!./venv/bin/python3
+#!/usr/bin/env python3
+
 from clickhouse_driver import Client
+import sys, psycopg2
+import modules.pg as pg
+import modules.ch as ch
 
-import sys
-import psycopg2
-import pg_migrate_and_insertion
-import ch_migrate_and_insertion
-
-DBNAME = "postgres";
-USER = "postgres";
-PASSWORD = "amin111";
+DBNAME = "perfdb";
+USER = "admin";
+PASSWORD = "password";
 HOST = "127.0.0.1";
-
-client = Client('localhost')
-
-
 
 
 if __name__ == "__main__":
+    chConn = Client(HOST)
+    pgConn = psycopg2.connect(dbname=DBNAME, user=USER, password=PASSWORD, host=HOST)
 
-	#подключение, создание таблицы и заполнение 
-    conn = psycopg2.connect(dbname=DBNAME, user=USER, password=PASSWORD, host=HOST)
     if len(sys.argv) > 1:
         if sys.argv[1] == "migrate":
-            pg_migrate_and_insertion.migrate();
-    pg_migrate_and_insertion.table_insert(conn);
-    
-    ch_migrate_and_insertion.migrate()
-    for i in range(500):
-        ch_migrate_and_insertion.insertion()
+            pg.migrate(pgConn)
+            ch.migrate(chConn)
+        elif sys.argv[1] == "fill":
+            pg.insert(pgConn)
+            ch.insert(chConn)
 
-    conn.commit()
-    conn.close()
-
+    pgConn.commit()
+    pgConn.close()
